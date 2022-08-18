4b92e49394c84c1bcb27a24af4046caef32ee7fd
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2022-08-18 23:37:15 +0900

    버스 기사 전용 Database ADT 구현 완료

--- Server/Database.py
+++ Server/Database.py
@@ -64,7 +64,7 @@ def createTable() -> None:
     print("[BUDDY-DB] 테이블 생성을 종료합니다.")
 
 
-class UserDB:
+class Database:
     """사용자의 정보를 등록하고 조회하기 위한 DB 인터페이스 클래스
     """
 
@@ -87,6 +87,8 @@ class UserDB:
             return
         print("[BUDDY-DB] 데이터베이스 연결 완료")
 
+    # USER Database method
+
     def addUser(self, name: str, phone_num: str, mac_add: str) -> bool:
         """데이터베이스에 사용자를 등록하기 위한 함수
 
@@ -95,6 +97,13 @@ class UserDB:
         :param mac_add: 추가할 사용자의 단말기 MAC 주소
         :return: boolean 사용자 등록 성공 여부 (True : 성공, False : 실패)
         """
+
+        if self.isUserExist(name, phone_num, mac_add) == 1:
+            print("[BUDDY-DB] 이미 존재하는 사용자입니다.")
+            return False
+
+        if self.isUserExist(name, phone_num, mac_add) == -1:
+            return False
 
         try:
             sql = "INSERT INTO usertable (phone, name, mac) VALUES (%s, %s, %s)"
@@ -116,6 +125,14 @@ class UserDB:
         :param mac_add: 삭제할 사용자의 단말기 MAC 주소
         :return: boolean 사용자 삭제 성공 여부 (True : 성공, False : 실패)
         """
+
+        if self.isUserExist(name, phone_num, mac_add) == 0:
+            print("[BUDDY-DB] 존재하지 않는 사용자입니다.")
+            return False
+
+        if self.isUserExist(name, phone_num, mac_add) == -1:
+            return False
+
         try:
             sql = "DELETE FROM usertable WHERE phone = %s AND name = %s AND mac = %s"
             self.cur.execute(sql, (phone_num,  name, mac_add))
@@ -128,7 +145,7 @@ class UserDB:
         print("[BUDDY-DB] 사용자 삭제 성공 -> 사용자 : ", name, phone_num, mac_add)
         return True
 
-    def getUserMac(self, name: str, phone_num: str) -> str:
+    def getUserMac(self, name: str, phone_num: str) -> str or None:
         """데이터베이스에서 사용자 이름과 전화번호를 이용하여 사용자의 단말기 Mac 주소를 검색하여 반환하는 함수
 
         :param name: 검색할 사용자의 이름
@@ -151,6 +168,131 @@ class UserDB:
 
         return result
 
+    def isUserExist(self, name: str, phone_num: str, mac_add: str) -> int:
+        """데이터베이스에서 해당 이름, 전화번호, 단말기 Mac 주소를 가지는 사용자가 있는지에 대한 여부를 반환한다.
+
+        :param name: 검색할 사용자의 이름
+        :param phone_num: 검색할 사용자의 전화번호
+        :param mac_add: 검색할 사용자의 단말기 Mac 주소
+        :return: int 존재할 경우 1, 존재하지 않을 경우 0, 그 밖의 오류는 -1을 반환한다.
+        """
+
+        try:
+            sql = "select EXISTS (select * from usertable where phone = %s AND name = %s " \
+                  "AND mac = %s limit 1) as success"
+            self.cur.execute(sql, (phone_num, name, mac_add))
+            self.conn.commit()
+            result = self.cur.fetchone()
+        except Exception as e:
+            print("[BUDDY-DB][ERR] 사용자 조회 실패 -> 사용자 : ", name, phone_num)
+            print(e.args[0])
+            return -1
+
+        return result[0]
+
+    # BUS DRIVER Database method
+
+    def addBusDriver(self, vehicleno: str, name: str, mac_add: str):
+        """데이터베이스에 버스 운전기사를 등록하기 위한 함수
+
+        :param name: 추가할 버스 운전기사의 이름
+        :param vehicleno: 추가할 버스 운전기사의 차량 번호
+        :param mac_add: 추가할 버스 운전기사의 단말기 MAC 주소
+        :return: boolean 사용자 등록 성공 여부 (True : 성공, False : 실패)
+        """
+
+        if self.isBusDriverExist(vehicleno, name, mac_add) == 1:
+            print("[BUDDY-DB] 이미 존재하는 버스 운전기사입니다.")
+            return False
+
+        if self.isBusDriverExist(vehicleno, name, mac_add) == -1:
+            return False
+
+        try:
+            sql = "INSERT INTO busdrivertable (vehicleno, name, mac) VALUES (%s, %s, %s)"
+            self.cur.execute(sql, (vehicleno, name, mac_add))
+            self.conn.commit()
+        except Exception as e:
+            print("[BUDDY-DB][ERR] 버스 운전기사 등록 실패 -> 버스 운전기사 : ", name, vehicleno, mac_add)
+            print(e.args[0])
+            return False
+
+        print("[BUDDY-DB] 버스 운전기사 등록 성공 -> 버스 운전기사 : ", name, vehicleno, mac_add)
+        return True
+
+    def removeBusDriver(self, vehicleno: str, name: str, mac_add: str) -> bool:
+        """데이터베이스에서 버스 운전기사를 삭제하기 위한 함수
+
+        :param name: 삭제할 버스 운전기사의 이름
+        :param vehicleno: 삭제할 버스 운전기사의 차량 번호
+        :param mac_add: 삭제할 버스 운전기사의 단말기 MAC 주소
+        :return: boolean 사용자 삭제 성공 여부 (True : 성공, False : 실패)
+        """
+
+        if self.isBusDriverExist(vehicleno, name, mac_add) == 0:
+            print("[BUDDY-DB] 존재하지 않는 버스 운전기사입니다.")
+            return False
+
+        if self.isBusDriverExist(vehicleno, name, mac_add) == -1:
+            return False
+
+        try:
+            sql = "DELETE FROM busdrivertable WHERE vehicleno = %s AND name = %s AND mac = %s"
+            self.cur.execute(sql, (vehicleno,  name, mac_add))
+            self.conn.commit()
+        except Exception as e:
+            print("[BUDDY-DB][ERR] 버스 운전기사 삭제 실패 -> 버스 운전기사 : ", name, vehicleno, mac_add)
+            print(e.args[0])
+            return False
+
+        print("[BUDDY-DB] 버스 운전기사 삭제 성공 -> 버스 운전기사 : ", name, vehicleno, mac_add)
+        return True
+
+    def getBusDriverMac(self, vehicleno: str, name: str) -> str or None:
+        """데이터베이스에서 버스 운전기사 이름과 차량 번호를 이용하여 버스 운전기사의 단말기 Mac 주소를 검색하여 반환하는 함수
+
+        :param vehicleno: 검색할 버스 운전기사의 차량 번호
+        :param name: 검색할 버스 운전기사의 이름
+        :return: str 버스 운전기사 단말기 Mac 주소 반환 (str : 버스 운전기사가 존재할 경우, None : 버스 운전기사가 등록되지 않았거나 실패한 경우)
+        """
+        try:
+            sql = "SELECT * FROM busdrivertable WHERE vehicleno = %s AND name = %s"
+            self.cur.execute(sql, (vehicleno,  name))
+            self.conn.commit()
+            result = self.cur.fetchone()
+        except Exception as e:
+            print("[BUDDY-DB][ERR] 버스 운전기사 조회 실패 -> 버스 운전기사 : ", name, vehicleno)
+            print(e.args[0])
+            return None
+
+        print("[BUDDY-DB] 버스 운전기사 조회 성공 -> 버스 운전기사 : ", result)
+        if result is not None:
+            result = result[2]
+
+        return result
+
+    def isBusDriverExist(self, vehicleno: str, name: str, mac_add: str) -> int:
+        """데이터베이스에서 해당 차량번호, 이름, 단말기 Mac 주소를 가지는 버스 운전기사가 있는지에 대한 여부를 반환한다.
+
+        :param vehicleno: 검색할 버스 운전기사의 차량 번호
+        :param name: 검색할 버스 운전기사의 이름
+        :param mac_add: 검색할 버스 운전기사의 단말기 Mac 주소
+        :return: int 존재할 경우 1, 존재하지 않을 경우 0, 그 밖의 오류는 -1을 반환한다.
+        """
+
+        try:
+            sql = "select EXISTS (select * from busdrivertable where vehicleno = %s AND name = %s " \
+                  "AND mac = %s limit 1) as success"
+            self.cur.execute(sql, (vehicleno,  name, mac_add))
+            self.conn.commit()
+            result = self.cur.fetchone()
+        except Exception as e:
+            print("[BUDDY-DB][ERR] 버스 운전기사 조회 실패 -> 버스 운전기사 : ", name, vehicleno)
+            print(e.args[0])
+            return -1
+
+        return result[0]
+
     def __del__(self) -> None:
         """ Database 클래스 소멸자. DB 인터페이스 객체가 사라짐에 따라
             데이터베이스를 저장하고 정상적으로 종료함
@@ -167,8 +309,21 @@ class UserDB:
 
 
 if __name__ == "__main__":
-    createTable()
-    db = UserDB()
+    #createTable()
+    db = Database()
+
+    print("\n")
+
     db.addUser("홍길동", "01012341234", "AA:AA:AA:AA:AA:AA")
-    result = db.getUserMac("홍길동", "01012341234")
+    db.addUser("홍길동", "01012341234", "AA:AA:AA:AA:AA:AA")
+    print(db.getUserMac('홍길동', '01012341234'))
     db.removeUser("홍길동", "01012341234", "AA:AA:AA:AA:AA:AA")
+    db.removeUser("홍길동", "01012341234", "AA:AA:AA:AA:AA:AA")
+
+    print("\n")
+
+    db.addBusDriver('경기17바1234', '기사님', 'BB:BB:BB:BB:BB:BB')
+    db.addBusDriver('경기17바1234', '기사님', 'BB:BB:BB:BB:BB:BB')
+    print(db.getBusDriverMac('경기17바1234', '기사님'))
+    db.removeBusDriver('경기17바1234', '기사님', 'BB:BB:BB:BB:BB:BB')
+    db.removeBusDriver('경기17바1234', '기사님', 'BB:BB:BB:BB:BB:BB')

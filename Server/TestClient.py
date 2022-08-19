cbdbc8c0e08e7456f09bd74486fe593d926841e6
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2022-08-19 18:50:02 +0900

    Timeout 테스트용 클라이언트

--- Server/TestClient.py
+++ Server/TestClient.py
@@ -1,5 +1,5 @@
 import asyncio
-
+import PROTOCOL as p
 
 async def run_client(host: str, port: int):
     # 서버와의 연결을 생성합니다.
@@ -10,19 +10,34 @@ async def run_client(host: str, port: int):
     print("[C] connected")
     # 루프를 돌면서 입력받은 내용을 서버로 보내고,
     # 응답을 받으면 출력합니다.
+
+    line = input("[C] enter message: ")
+    # 입력받은 내용을 서버로 전송
+
+    payload = line.encode()
+    writer.write(payload)
+    await writer.drain()
+
+    print(f"[C] sent: {len(payload)} bytes.\n")
+
+    test_num = 0
+
     while True:
-        line = input("[C] enter message: ")
-        if not line:
-            break
-        # 입력받은 내용을 서버로 전송
-        payload = line.encode()
-        writer.write(payload)
-        await writer.drain()
-        print(f"[C] sent: {len(payload)} bytes.\n")
+
         # 서버로부터 받은 응답을 표시
         data = await reader.read(1024)  # type: bytes
         print(f"[C] received: {len(data)} bytes")
         print(f"[C] message: {data.decode()}")
+
+        if data.decode() == p.CONNECTION_CHECK and test_num < 5:
+            payload = p.CONNECTION_CHECK
+            writer.write(payload.encode())
+            await writer.drain()
+            test_num += 1
+
+        if data.decode() == p.KICK_USER:
+            break
+
     # 연결을 종료합니다.
     print("[C] closing connection...")
     writer.close()

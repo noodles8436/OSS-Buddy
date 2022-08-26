7e33b074792d85509fd58ad1c0ca6738ca435188
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2022-08-26 15:47:07 +0900

    버스 데이터 파일이 담긴 파일 초기값 설정 함수 구현

--- RaspberryPI/BusManager.py
+++ RaspberryPI/BusManager.py
@@ -1,13 +1,24 @@
+import FileManager
 
 
 class BusManager:
 
     def __init__(self):
-        self.BusData = dict()
+        self.BusData = FileManager.configManager()
+        if self.BusData.isKey("cityCode") is False or self.BusData.isKey("nodeID") is False:
+            print('[RaspBerry PI][ERR] 초기값을 설정해야 합니다!')
+            print('[RaspBerry PI] cityCode : ', end='')
+            cityCode = input()
+            print('[RaspBerry PI] nodeID : ', end='')
+            nodeID = input()
+            self.resetBusStop(new_CityCode=cityCode, new_NodeId=nodeID)
+
         pass
 
     def resetBusStop(self, new_CityCode: str, new_NodeId: str):
-        pass
+        self.BusData.setValue('cityCode', new_CityCode)
+        self.BusData.setValue('nodeID', new_NodeId)
+        print(f'[RaspBerry PI] cityCode : {new_CityCode} , nodeID : {new_NodeId} 로 설정되었습니다.')
 
     def addBusRoute(self, cityCode: str, routeId: str, routeNo: str, vehicleNo: str) -> bool:
         pass

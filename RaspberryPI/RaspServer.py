663a7080cbd53e766064608aa878d7af564d3809
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2022-08-30 19:49:43 +0900

    GPS 체제로 바뀐 변동사항 적용 및 Detector, InfoProvider 구현중
    busDetector 내부 로그인 이후 while 문에서 아래의 작업을 수행함.
    서버에게 지속적으로 탐지를 통해 얻어낸 결과를 비동기적으로 전달함
    서버는 메세지를 받을 뿐 답장을 하지 않음.
    
    InfoProvider 내부 로그인 이후 while문에서 아래의 작업을 수행함
    서버로부터 요청사항을 기다린 후, 요청사항을 처리하고 그 결과를 서버에게 전달함
    서버로부터 요청사항이 올때가지 해당 async 함수는 wait 상태임.
    
    2개의 비동기 소켓을 통해 서버와 통신하며, 이에 따른 분기된 PROTOCOL 방식이 필요함.

--- RaspberryPI/RaspServer.py
+++ RaspberryPI/RaspServer.py
@@ -1,12 +1,85 @@
+import asyncio
+import Server.PROTOCOL as p
+import BusManager
+import Detector
 
 
-class RaspServer:
+class RaspMain:
 
-    def __init__(self):
-        pass
-
-    async def deviceFinder(self):
-        pass
+    def __init__(self, host: str, port: int):
+        self.busManager = BusManager.BusManager()
+        self.Detector = Detector.Detector()
+        self.host = host
+        self.port = port
 
     async def busDetector(self):
-        pass
+        reader: asyncio.StreamReader
+        writer: asyncio.StreamWriter
+
+        while True:
+            try:
+                print("[Rasp Detector] Try to Connect Server")
+                reader, writer = await asyncio.open_connection(host=self.host, port=self.port)
+                print("[Rasp Detector] Server Connected")
+
+                msg = p.RASP_DETECTOR_LOGIN + p.TASK_SPLIT + self.busManager.getNodeId()
+                writer.write(msg.encode())
+                await writer.drain()
+                print("[Rasp Detector] Try to Login Server")
+
+                recv = await reader.read(p.SERVER_PACKET_SIZE)
+
+                if recv != p.RASP_DETECTOR_LOGIN_SUCCESS:
+                    print("[Rasp Detector][ERR] Server Login Failed!")
+                    continue
+
+                print("[Rasp Detector] Server Login Success")
+
+                while True:
+                    # Detecting Bus and send to Server
+                    pass
+
+            except Exception as e:
+                print(e.args[0])
+                if writer is not None:
+                    await writer.drain()
+                    writer.close()
+                    await writer.wait_closed()
+
+    async def infoProvider(self):
+        reader: asyncio.StreamReader
+        writer: asyncio.StreamWriter
+
+        while True:
+            try:
+                print("[Rasp INFO] Try to Connect Server")
+                reader, writer = await asyncio.open_connection(host=self.host, port=self.port)
+                print("[Rasp INFO] Server Connected")
+
+                msg = p.RASP_INFO_LOGIN + p.TASK_SPLIT + self.busManager.getNodeId()
+                writer.write(msg.encode())
+                await writer.drain()
+                print("[Rasp INFO] Try to Login Server")
+
+                recv = await reader.read(p.SERVER_PACKET_SIZE)
+
+                if recv != p.RASP_INFO_LOGIN_SUCCESS:
+                    print("[Rasp INFO][ERR] Server Login Failed!")
+                    continue
+
+                print("[Rasp INFO] Server Login Success")
+
+                while True:
+                    # Repeat : listening server and answer to server
+                    pass
+
+            except Exception as e:
+                print(e.args[0])
+                if writer is not None:
+                    await writer.drain()
+                    writer.close()
+                    await writer.wait_closed()
+
+
+if __name__ == "__main__":
+    pass

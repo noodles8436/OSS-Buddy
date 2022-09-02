98be1e2c01bb960ed3b30a4f950c7942fe8e7fcb
runyeong <58145940+jooyoung39@users.noreply.github.com>
2022-09-02 16:32:33 +0900

    애플리케이션 안내 및 로딩
    애플리케이션 안내 및 로딩창 구현

---
+++ Loading_page.java
@@ -0,0 +1,27 @@
+package com.djy.budy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Loading_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_page);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+               // Intent intent = new Intent(getBaseContext(), User_select.class);
+               // startActivity(intent);
+                 startActivity(new Intent(Loading_page.this,LoginActivity.class));
+                finish();
+            }
+        }, 2000);
+
+
+    }
+}

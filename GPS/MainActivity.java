28efa4caa4e4d9c5fa8a34ec99d60e4eb0c3e93a
coyadon <83261894+coyadon@users.noreply.github.com>
2022-09-06 23:27:49 +0900

    changebusname후 필요한 예/아니오 버튼 레이아웃
    주석처리 보고 수정하셔서 적용하시면 됩니다.

--- GPS/MainActivity.java
+++ GPS/MainActivity.java
@@ -1,41 +1,31 @@
-package com.example.myapplication;
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.speech.tts.TextToSpeech;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+package com.example.ynbutton;
 
 import static android.speech.tts.TextToSpeech.ERROR;
 
-import java.io.IOException;
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private TextToSpeech tts;
-    private TextView txtResult;
 
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        //이전레이아웃자리에 이 화면으로 넘어오기 전 레이아웃을 입력하면 됩니다.
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.이전레이아웃);
+        View view = inflater.inflate(R.layout.이전레이아웃);
+        //이전레이아웃에 있는 id:reservation을 가진 버튼을 가져옵니다.
+        Button busname = (Button) view.findViewById(R.id.reservation);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -45,34 +35,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        Button ybutton = (Button) findViewById(R.id.yesbutton);
+        Button nbutton = (Button) findViewById(R.id.nobutton);
+         ybutton.setOnClickListener(new DoubleClickListener() {
+             @Override
+             public void onDoubleClick() {
+                 //버스 예약된 화면으로 넘어가기
+             }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button1 = (Button) findViewById(R.id.button1);
-        txtResult = (TextView) findViewById(R.id.txtResult);
-        double[] loca = {-2, -2};
-        loca = GPS();
-        txtResult.setText(loca[0] + "\n" + loca[1]);
+             @Override
+             public void onSingleClick() {
+                 tts.speak(String.valueOf(busname.getText()+"이 맞으십니까? 맞다면 이 부분을 더블클릭해주세요"), TextToSpeech.QUEUE_FLUSH,null);
+             }
+         });
+         nbutton.setOnClickListener(new DoubleClickListener() {
+             @Override
+             public void onDoubleClick() {
+                //이전 버스 찾는 화면으로 넘어가기
+             }
 
-    }
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public double[] GPS() {
-        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        double[] loca = new double[2];
-        double[] failure = {-1, -1};
-        if (Build.VERSION.SDK_INT >= 23 &&
-                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // vibrator.vibrate(500);
-            VibrationEffect vibe = VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE);
-            //vibrator.vibrate(VibrationEffect.EFFECT_TICK, );
-            tts.speak("허용을 눌러 권한 설정을 해주세요.", TextToSpeech.QUEUE_FLUSH, null);
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    0);
-        } else {
-                Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                loca[0] = location.getLongitude();
-                loca[1] = location.getLatitude();
-                return loca;
-        }return loca;
+             @Override
+             public void onSingleClick() {
+                 tts.speak(String.valueOf(busname.getText()+"이 아니십니까? 예약하고자 하는 버스가 아니라면 이 부분을 더블클릭해주세요"), TextToSpeech.QUEUE_FLUSH,null);
+             }
+         });
     }
 }

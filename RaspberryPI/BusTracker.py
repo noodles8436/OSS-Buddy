80f6dfdc19bc61cca169d88554c5978610bc8aaa
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2022-09-13 14:25:09 +0900

    Update BusTracker.py

--- RaspberryPI/BusTracker.py
+++ RaspberryPI/BusTracker.py
@@ -38,6 +38,7 @@ class BusTracker:
         queryParams = baseParams
 
         xml = requests.get(URL_getCtyCodeList + queryParams).text
+        print(xml)
         root = bs(xml, features="xml")
         resultCode = root.find("resultCode").get_text()
         resultMsg = root.find("resultMsg").get_text()
@@ -111,7 +112,7 @@ class BusTracker:
             nodeItem['gpslati'] = float(item.find('gpslati').get_text())
             nodeItem['gpslong'] = float(item.find('gpslong').get_text())
             nodeItem['nodeid'] = item.find('nodeid').get_text()
-            # nodeItem['nodeno'] = int(item.find('nodeno').get_text())
+            nodeItem['nodeno'] = int(item.find('nodeno').get_text())
             nodeItem['nodeord'] = int(item.find('nodeord').get_text())
             nodeItem['nodenm'] = item.find('nodenm').get_text()
             result['nodeDict'][str(nodeItem['nodeord'])] = nodeItem
@@ -122,10 +123,13 @@ class BusTracker:
 
     def getAllBusinRoute(self, cityCode: str, routeId: str) -> dict or None:
         queryParams = baseParams + "&cityCode=" + cityCode + "&routeId=" + routeId + \
-                      "&numOfRows=-1" + "&pageNo=1"
+                      "&numOfRows=20" + "&pageNo=1"
 
         xml = requests.get(URL_getRouteAcctoBusLcList + queryParams).text
         root = bs(xml, features="xml")
+
+        print(root)
+
         resultCode = root.find("resultCode").get_text()
         resultMsg = root.find("resultMsg").get_text()
 
@@ -150,7 +154,7 @@ class BusTracker:
             locations.append(busItem['nodeord'])
             busItem['nodenm'] = item.find('nodenm').get_text()
             busItem['routetp'] = item.find('routetp').get_text()
-            busItem['vehicleno'] = item.find('vehicleno').get_text()
+            busItem['vehicleNo'] = item.find('vehicleno').get_text()
             result['busDict'][str(busItem['nodeord'])] = busItem
 
         result['locations'] = locations
@@ -161,10 +165,13 @@ class BusTracker:
 
     def getSttnNoList(self, cityCode: str, nodeNm: str, nodeNo: str) -> dict or None:
         queryParams = baseParams + "&cityCode=" + cityCode + "&nodeNm=" + nodeNm + \
-                      "&nodeNo=" + nodeNo + "&numOfRows=-1" + "&pageNo=1"
+                      "&nodeNo=" + nodeNo + "&numOfRows=1" + "&pageNo=1"
 
         xml = requests.get(URL_getSttnNoList + queryParams).text
         root = bs(xml, features="xml")
+
+        print(xml)
+
         resultCode = root.find("resultCode").get_text()
         resultMsg = root.find("resultMsg").get_text()
 
@@ -188,7 +195,7 @@ class BusTracker:
 
     def getSttnThrghRouteList(self, cityCode: str, nodeId: str) -> dict or None:
         queryParams = baseParams + "&cityCode=" + cityCode + "&nodeid=" + nodeId + \
-                      "&numOfRows=-1" + "&pageNo=1"
+                      "&numOfRows=50" + "&pageNo=1"
 
         xml = requests.get(URL_getSttnThrghRouteList + queryParams).text
         root = bs(xml, features="xml")
@@ -216,9 +223,16 @@ class BusTracker:
 
 if __name__ == "__main__":
     BT = BusTracker()
-    # print(BT.getAllServiceAreas()) #지역 코드 얻기
-    # print(BT.getBusInformation('32020', '34')) #GGB234000021 #GGB234000026 (노선번호)
-    print(BT.getBusThrghSttnList('32020', 'WJB251000082'))  # 오리역 GGB206000215 GGB206000040 (정류장 번호)
+    #print(BT.getAllServiceAreas()) #지역 코드 얻기
+    #print(BT.getBusInformation('32020', '30')) # 30번 : WJB251000068
+    #print(BT.getBusThrghSttnList('32020', 'WJB251000068'))
+    #print(BT.getSttnNoList(cityCode='32020', nodeNm='연세대 복지타운', nodeNo='36043'))
+    # 연세대 복지타운 : WJB251036043 , 51번 순서 정류장, lati:37.277093, long:127.903682
+    # nodeno : 36043
+
+    #print(BT.getSttnThrghRouteList(cityCode='32020', nodeId='WJB251036035'))
+    # 세동마을 : WJB251036035, nodeno: 36035, lati:37.280122 long:127.91001 46번 정류장
+
     # print(BT.getAllBusinRoute('31250', 'GGB234000021'))
 
     # cityCode는 어케 얻지?

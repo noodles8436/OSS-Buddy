776ac983b9ba2469937bc932ddf1441eb5c81711
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2022-09-13 21:49:21 +0900

    Add files via upload

---
+++ main/ossBusDriver/BusReserve.java
@@ -0,0 +1,21 @@
+package ossBusDriver;
+
+public class BusReserve {
+	
+	private String stationName;
+	private int ArrivalLeftNode;
+	
+	public BusReserve(String stationName, int ArrivalLeftNode) {
+		this.stationName = stationName;
+		this.ArrivalLeftNode = ArrivalLeftNode;
+	}
+	
+	public String getStationName() {
+		return this.stationName;
+	}
+	
+	public int getArrivalLeftNode() {
+		return this.ArrivalLeftNode;
+	}
+	
+}
---
+++ main/ossUser/BusArrival.java
@@ -0,0 +1,21 @@
+package ossUser;
+
+public class BusArrival {
+	
+	private String routeNo;
+	private int arrivalLeftNode;
+	
+	public BusArrival(String routeNo, int arrivalLeftNode) {
+		this.routeNo = routeNo;
+		this.arrivalLeftNode = arrivalLeftNode;
+	}
+	
+	public String getRouteNo() {
+		return this.routeNo;
+	}
+	
+	public int arrivalLeftNode() {
+		return this.arrivalLeftNode;
+	}
+	
+}

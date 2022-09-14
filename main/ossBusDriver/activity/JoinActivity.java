370aec7fed272f4c50b523214cc9da9ab6de6243
coyadon <83261894+coyadon@users.noreply.github.com>
2022-09-14 23:01:57 +0900

    버스기사 회원가입(업데이트)
    회원가입을 할 때, 이름,휴대전화번호만 입력하는 것에서 휴대전화번호를 빼고 차량번호와 노선번호까지 입력하는 것으로 바꾸었습니다. 또한 10자리의 무작위 숫자를 통해 버스기사의 UID를 만들었습니다.

--- main/ossBusDriver/activity/JoinActivity.java
+++ main/ossBusDriver/activity/JoinActivity.java
@@ -2,12 +2,15 @@ package com.example.realbusdriver;
 
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.net.Network;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.RandomAccessFile;
+import java.util.Random;
+import java.util.RandomAccess;
 
 public class JoinActivity extends AppCompatActivity{
     @Override
@@ -17,19 +20,38 @@ public class JoinActivity extends AppCompatActivity{
     }
     public void joinbtn_Click(View view){
         EditText bdname=(EditText) findViewById(R.id.name);
-        EditText bdtel=(EditText) findViewById(R.id.busdnum);
+        EditText vehicle=(EditText) findViewById(R.id.vehiclenum);
+        EditText routenum=(EditText)findViewById(R.id.routenum);
         new AlertDialog.Builder(this)
                 .setTitle("회원가입")
-                .setMessage(bdname.getText()+"\n"+bdtel.getText()+"로 회원가입하시겠습니까?")
+                .setMessage("이름:"+bdname.getText()+"\n"+vehicle.getText()+"\n"+routenum.getText()+"로 회원가입하시겠습니까?")
                 .setIcon(android.R.drawable.ic_menu_save)
                 .setPositiveButton(android.R.string.yes, (dialogInterface, i) -> {
+                    Random random=new Random();
+                    long UID_first=random.nextInt(9999)+1;
+                    long UID_second=random.nextInt(999999)+1;
+                    String UID= String.valueOf(UID_first)+UID_second;
+
                     NetworkBus.setDriverName(bdname.getText().toString());
-                    NetworkBus.setDriverUID(bdtel.getText().toString());
-                    NetworkBus.setDriverVehicleNo("강원12자3948");
+                    NetworkBus.setDriverVehicleNo(BuscheckId(vehicle.getText().toString()));
+                    NetworkBus.setDriverRouteNo(routenum.getText().toString());
+                    NetworkBus.setDriverUID(UID);
                     NetworkBus.register();
                     Intent intent = new Intent(this,MainActivity.class);
                     startActivity(intent);
                 })
                 .setNegativeButton(android.R.string.no, (dialogInterface, i) -> Toast.makeText(getApplicationContext(),"취소하였습니다. 다시 입력해주세요",Toast.LENGTH_SHORT).show()).show();
+    }
+    public String BuscheckId(String id){
+        String tmpid = id;
+        if(tmpid.replace(" ","").equals("")){
+            Toast.makeText(getApplicationContext(),"공백만 입력되었습니다. 다시 입력해주세요",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            tmpid = tmpid.replace(" ", "");
+            return tmpid;
+        }
+        return null;
     }
 }

ee750a1e21e61bc869baced2d16f68f2882fd9a1
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2022-09-15 01:12:36 +0900

    Update Model.py

--- RaspberryPI/Model.py
+++ RaspberryPI/Model.py
@@ -20,25 +20,28 @@ class Model:
         detections = self.model(cvImg)
         results = detections.pandas().xyxy[0].to_dict(orient="records")
         _pred: list = list()
-
         for result in results:
             if result['class'] != YOLO_BUS_LABEL or result['confidence'] < confidence:
                 continue
-
             x1 = int(result['xmin'])
             y1 = int(result['ymin'])
             x2 = int(result['xmax'])
             y2 = int(result['ymax'])
-
             cropImg = cvImg[y1:int(y2 / 2), x1:x2, :]
             cropImg = cv2.cvtColor(cropImg, cv2.COLOR_BGR2GRAY)
             # cv2.imshow('Final', cropImg)
             # cv2.waitKey(0)
-            result = self.reader.readtext(cropImg)
+            ocr_result = self.reader.readtext(cropImg)
             _data: list = list()
-            for (bbox, text, prob) in result:
-                _data.append([text, prob, (x1+x2) // 2])
 
+            if len(ocr_result) == 0:
+                continue
+
+            print(ocr_result)
+
+            for (bbox, text, prob) in ocr_result:
+                _data.append([text, prob, (x1+x2) // 2])
+            print("data : ", _data)
             _pred.append(_data)
 
         if len(_pred) == 0:

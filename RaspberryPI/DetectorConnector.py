3d707cd1c6c367629372feaf07260c1de1f1071b
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2022-11-09 11:31:20 +0900

    Update DetectorConnector.py

--- RaspberryPI/DetectorConnector.py
+++ RaspberryPI/DetectorConnector.py
@@ -1,6 +1,9 @@
 import asyncio
+import socket
+import time
+
 from Assignment import DetectResult, getDumpFromObject, loadDataFromDump
-from PROTOCOL import GLOBAL_SERVER_PACKET_SIZE
+from PROTOCOL import *
 import numpy as np
 
 
@@ -11,41 +14,52 @@ class DetectorConnector:
         self.reader = None
         self.globalServer_ip = ip
         self.globalServer_port = port
+        self.client_socket = None
+        self.connectServer()
 
-        asyncio.run(self.connectServer())
+    def connectServer(self):
+        while True:
+            print("[Raspberry][GLOBALSERVER] Try to Connect to Global Server")
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((self.globalServer_ip, self.globalServer_port))
+            self.client_socket = client_socket
 
-        print('[Detector-Connector] reader -> ', self.reader)
-        print('[Detector-Connector] writer -> ', self.writer)
+            print("[Raspberry][GLOBALSERVER] Global Server Connected!")
+
+            msg = CLIENT_RASPBERRY_LOGIN
+            self.client_socket.sendall(msg.encode())
+
+            print('[Raspberry][GLOBALSERVER] Try to Login Global Server')
+
+            recv: bytes = client_socket.recv(GLOBAL_SERVER_PACKET_SIZE)
+            _msg: str = str(recv.decode())
+
+            print(' SERVER RECEIVED : ', _msg)
+            print(CLIENT_LOGIN_SUCCESS)
+
+            if _msg != CLIENT_LOGIN_SUCCESS:
+                print("[Raspberry][GLOBALSERVER] Global Server Login Failed!")
+                time.sleep(5)
+                continue
+
+            print("[Raspberry][GLOBALSERVER] Global Server Login Success!")
+            break
+
+    def disconnectServer(self):
+        if self.client_socket is not None:
+            self.client_socket.close()
 
     def detect(self, images: np.ndarray) -> DetectResult:
-        return asyncio.run(self.run(images=images))
-
-    async def connectServer(self):
-        reader, writer = await asyncio.open_connection(host=self.globalServer_ip, port=self.globalServer_port)
-        self.reader = reader
-        self.writer = writer
-
-    async def disconnectServer(self):
-        if self.writer is not None:
-            try:
-                await self.writer.drain()
-                self.writer.close()
-                await self.writer.wait_closed()
-            except Exception:
-                pass
-
-        self.writer = None
-        self.reader = None
-
-    async def run(self, images: np.ndarray) -> DetectResult:
-        self.writer: asyncio.StreamWriter
-        self.reader: asyncio.StreamReader
+        import sys
         bytes_img: bytes = getDumpFromObject(images)
+        print(sys.getsizeof(bytes_img))
+        self.client_socket.send(bytes_img)
 
-        self.writer.write(bytes_img)
-        await self.writer.drain()
-
-        bytes_result: bytes = await self.reader.read(GLOBAL_SERVER_PACKET_SIZE)
+        print(' IMAGES SENT ! ')
+        bytes_result: bytes = self.client_socket.recv(GLOBAL_SERVER_PACKET_SIZE)
+        # bytes_result: bytes = await self.reader.read(GLOBAL_SERVER_PACKET_SIZE)
         detectResult: DetectResult = loadDataFromDump(bytes_result)
+
+        print(' Result Received! ')
 
         return detectResult
